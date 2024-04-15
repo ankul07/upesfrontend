8a4c5d10f975148d5e9fe90e22e896fa9c42a73d
ankul07 <96623109+ankul07@users.noreply.github.com>
2024-04-15 23:52:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
       <h1 className="text-lg md:text-xl font-extrabold">
-        <Link to="/">Blog Market</Link>
+        <Link to="/">UPES Blog Mania</Link>
       </h1>
       {path === "/" && (
         <div className="flex justify-center items-center space-x-0">
